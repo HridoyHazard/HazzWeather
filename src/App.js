f66e5d8b01dd5ba90fc54949e27d44a9cf20ae86
Ashmit JaiSarita Gupta <43639341+devilkiller-ag@users.noreply.github.com>
2022-10-11 15:39:00 +0530

    Removed Space between App name

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ function App() {
         >
           <div className="name">
             <div className="logo">
-              Hazz Weather<hr></hr>
+              HazzWeather<hr></hr>
             </div>
             <div className="toggle-container">
               <input
