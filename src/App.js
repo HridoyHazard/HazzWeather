22926f4724a96e3c8ce6091bc5c5c72d366e2fba
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2022-10-15 05:54:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -226,6 +226,7 @@ function App() {
               <option value='fr'>Français</option>
               <option value='id'>Indonesia</option>
               <option value='ta'>தமிழ்</option>
+              <option value='bn'>বাংলা</option>
               <option value='zh'>简体中文</option>
               <option value='es'>{t('languages.es')}</option>
               <option value='fr'>{t('languages.fr')}</option>
