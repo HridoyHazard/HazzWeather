a5325476fb8c40720d26d3d5d783aa1439b4b6e0
Geoffrey Kasuba Sichali <Kasubasich@gmail.com>
2022-10-22 15:37:21 +0200

    fixing failed compile

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {
   TbVolume,
   TbVolumeOff,
 } from 'react-icons/tb';
-import Animation from './components/Animation';
 import DetailsCard from './components/DetailsCard';
 import SummaryCard from './components/SummaryCard';
 import './languages/i18n';
@@ -19,6 +18,7 @@ import Astronaut from './asset/not-found.svg';
 import SearchPlace from './asset/search.svg';
 import BackgroundColor from './components/BackgroundColor';
 import BackgroundImage from './components/BackgroundImage';
+import Animation from './components/Animation';
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
@@ -130,146 +130,97 @@ function App() {
   window.addEventListener('load', function () {
     navigator.geolocation.getCurrentPosition(myIP);
   });
-
   return (
-    <div>
-      <script>
-        <Animation/>
-      </script>
-      <div className='container'>
+    <div className='container'>
+      <div
+        className='blur'
+        style={{
+          background: `${weatherData ? BackgroundColor(weatherData) : '#a6ddf0'
+            }`,
+          top: '-10%',
+          right: '0',
+        }}
+      ></div>
+      <div
+        className='blur'
+        style={{
+          background: `${weatherData ? BackgroundColor(weatherData) : '#a6ddf0'
+            }`,
+          top: '36%',
+          left: '-6rem',
+        }}
+      ></div>
+      <div className='content'>
         <div
-          className='blur'
+          className='form-container'
           style={{
-            background: `${weatherData ? BackgroundColor(weatherData) : '#a6ddf0'
-              }`,
-            top: '-10%',
-            right: '0',
+            backgroundImage: `url(${weatherData ? BackgroundImage(weatherData) : LakeBackground
+              })`,
           }}
-        ></div>
-        <div
-          className='blur'
-          style={{
-            background: `${weatherData ? BackgroundColor(weatherData) : '#a6ddf0'
-              }`,
-            top: '36%',
-            left: '-6rem',
-          }}
-        ></div>
-        <div className='content'>
-          <div
-            className='form-container'
-            style={{
-              backgroundImage: `url(${weatherData ? BackgroundImage(weatherData) : LakeBackground
-                })`,
-            }}
-          >
-            <div className='name'>
-              <div className='logo'>
-                Hazz Weather<br></br>
-              </div>
-              <div className='toggle-container'>
-                <input
-                  type='checkbox'
-                  className='checkbox'
-                  id='checkbox'
-                  onChange={toggleDark}
+        >
+          <div className='name'>
+            <Animation/>
+            <div className='toggle-container'>
+              <input
+                type='checkbox'
+                className='checkbox'
+                id='checkbox'
+                onChange={toggleDark}
+              />
+              <label htmlFor='checkbox' className='label'>
+                <TbMoon
+                  style={{
+                    color: '#a6ddf0',
+                  }}
                 />
-                <label htmlFor='checkbox' className='label'>
-                  <TbMoon
-                    style={{
-                      color: '#a6ddf0',
-                    }}
-                  />
-                  <TbSun
-                    style={{
-                      color: '#f5c32c',
-                    }}
-                  />
-                  <div className='ball' />
-                </label>
-              </div>
-              <div className='city'>
-                <TbMapSearch />
-                <p>{city ?? t('unknown-location')}</p>
-              </div>
+                <TbSun
+                  style={{
+                    color: '#f5c32c',
+                  }}
+                />
+                <div className='ball' />
+              </label>
             </div>
-            <div className='search'>
-              <h2
-                style={{
-                  marginRight: currentLanguage === 'es' || 'fr' ? '10px' : '0px',
-                }}
-              >
-                {t('title')}
-              </h2>
-
-              <hr />
-              <form className='search-bar' noValidate onSubmit={handleSubmit}>
-                <input
-                  type='text'
-                  name=''
-                  id=''
-                  onClick={activate}
-                  placeholder={active ? '' : 'Explore cities weather'}
-                  onChange={handleChange}
-                  required
-                />
-                <button className='s-icon'>
-                  <TbSearch
-                    onClick={() => {
-                      navigator.geolocation.getCurrentPosition(myIP);
-                    }}
-                  />
-                </button>
-              </form>
-              <button
-                className='s-icon sound-toggler'
-                onClick={() => setBackgroundSoundEnabled((prev) => !prev)}
-              >
-                {backgroundSoundEnabled ? <TbVolume /> : <TbVolumeOff />}
-              </button>
+            <div className='city'>
+              <TbMapSearch />
+              <p>{city ?? t('unknown-location')}</p>
             </div>
           </div>
-          <div className='info-container'>
-            <div className='info-inner-container'>
-              <select
-                className='selected-languange'
-                defaultValue={currentLanguage}
-                onChange={(e) => handleLanguage(e)}
-              >
-                <option selected value='en'>
-                  English
-                </option>
-                <option value='es'>Español</option>
-                <option value='fr'>Français</option>
-                <option value='id'>Indonesia</option>
-                <option value='ta'>தமிழ்</option>
-                <option value='bn'>বাংলা</option>
-                <option value='zh'>简体中文</option>
-                <option value='ukr'>Ukrainian</option>
-                <option value='es'>{t('languages.es')}</option>
-                <option value='fr'>{t('languages.fr')}</option>
-                <option value='id'>{t('languages.id')}</option>
-                <option value='it'>{t('languages.it')}</option>
-                <option value='ta'>{t('languages.ta')}</option>
-                <option value='bn'>{t('languages.bn')}</option>
-                <option value='zh'>{t('languages.zh')}</option>
-                <option value='ptBR'>{t('languages.ptBR')}</option>
-                <option value='neNP'>{t('languages.neNP')}</option>
-                <option value='he'>{t('languages.he')}</option>
-              </select>
-              <div className='toggle-container'>
-                <input
-                  type='checkbox'
-                  className='checkbox'
-                  id='fahrenheit-checkbox'
-                  onChange={toggleFahrenheit}
+          <div className='search'>
+            <h2
+              style={{
+                marginRight: currentLanguage === 'es' || 'fr' ? '10px' : '0px',
+              }}
+            >
+              {t('title')}
+            </h2>
+
+            <hr />
+            <form className='search-bar' noValidate onSubmit={handleSubmit}>
+              <input
+                type='text'
+                name=''
+                id=''
+                onClick={activate}
+                placeholder={active ? '' : 'Explore cities weather'}
+                onChange={handleChange}
+                required
+              />
+              <button className='s-icon'>
+                <TbSearch
+                  onClick={() => {
+                    navigator.geolocation.getCurrentPosition(myIP);
+                  }}
                 />
-                <label htmlFor='fahrenheit-checkbox' className='label'>
-                  <RiFahrenheitFill />
-                  <RiCelsiusFill />
-                  <div className='ball' />
-                </label>
-              </div>
+              </button>
+            </form>
+            <button
+              className='s-icon sound-toggler'
+              onClick={() => setBackgroundSoundEnabled((prev) => !prev)}
+            >
+              {backgroundSoundEnabled ? <TbVolume /> : <TbVolumeOff />}
+            </button>
+          </div>
         </div>
         <div className='info-container'>
           <div className='info-inner-container'>
@@ -311,62 +262,62 @@ function App() {
                 <div className='ball' />
               </label>
             </div>
-            {loading ? (
-              <div className='loader'></div>
-            ) : (
-              <span>
-                {weatherData.length === 0 ? (
-                  <div className='nodata'>
-                    <h1>{noData ?? t('no-data')}</h1>
-                    {noData === 'Location Not Found' ? (
-                      <>
-                        <img
-                          src={Astronaut}
-                          alt='an astronaut lost in the space'
-                        />
-                        <p>Oh oh! We're lost in space finding that place.</p>
-                      </>
-                    ) : (
-                      <>
-                        <img
-                          src={SearchPlace}
-                          alt='a person thinking about what place to find'
-                        />
-                        <p style={{ padding: '20px' }}>
-                          Don't worry, if you don't know what to search for, try:
-                          Dhaka, Canada or maybe USA.
-                        </p>
-                      </>
-                    )}
-                  </div>
-                ) : (
-                  <>
-                    <h1 className='centerTextOnMobile'>{t('today')}</h1>
-                    <DetailsCard
-                      weather_icon={weatherIcon}
-                      data={weatherData}
-                      soundEnabled={backgroundSoundEnabled}
-                      isFahrenheitMode={isFahrenheitMode}
-                      degreeSymbol={degreeSymbol}
-                    />
-                    <h1 className='title centerTextOnMobile'>
-                      {t('more-on')} {city ?? t('unknown-location')}
-                    </h1>
-                    <ul className='summary'>
-                      {weatherData.list.map((days, index) => (
-                        <SummaryCard
-                          key={index}
-                          day={days}
-                          isFahrenheitMode={isFahrenheitMode}
-                          degreeSymbol={degreeSymbol}
-                        />
-                      ))}
-                    </ul>
-                  </>
-                )}
-              </span>
-            )}
           </div>
+          {loading ? (
+            <div className='loader'></div>
+          ) : (
+            <span>
+              {weatherData.length === 0 ? (
+                <div className='nodata'>
+                  <h1>{noData ?? t('no-data')}</h1>
+                  {noData === 'Location Not Found' ? (
+                    <>
+                      <img
+                        src={Astronaut}
+                        alt='an astronaut lost in the space'
+                      />
+                      <p>Oh oh! We're lost in space finding that place.</p>
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src={SearchPlace}
+                        alt='a person thinking about what place to find'
+                      />
+                      <p style={{ padding: '20px' }}>
+                        Don't worry, if you don't know what to search for, try:
+                        Dhaka, Canada or maybe USA.
+                      </p>
+                    </>
+                  )}
+                </div>
+              ) : (
+                <>
+                  <h1 className='centerTextOnMobile'>{t('today')}</h1>
+                  <DetailsCard
+                    weather_icon={weatherIcon}
+                    data={weatherData}
+                    soundEnabled={backgroundSoundEnabled}
+                    isFahrenheitMode={isFahrenheitMode}
+                    degreeSymbol={degreeSymbol}
+                  />
+                  <h1 className='title centerTextOnMobile'>
+                    {t('more-on')} {city ?? t('unknown-location')}
+                  </h1>
+                  <ul className='summary'>
+                    {weatherData.list.map((days, index) => (
+                      <SummaryCard
+                        key={index}
+                        day={days}
+                        isFahrenheitMode={isFahrenheitMode}
+                        degreeSymbol={degreeSymbol}
+                      />
+                    ))}
+                  </ul>
+                </>
+              )}
+            </span>
+          )}
         </div>
       </div>
     </div>
